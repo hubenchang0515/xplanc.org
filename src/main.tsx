3cd1990c18a7921154addcceddab8668da8ed98b
Plan C <hubenchang0515@outlook.com>
2025-06-04 13:52:31 +0800

    首页自动跳转

---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+if (navigator.language === 'zh-CN') {
+    window.location.replace("https://xplanc.org/primers/document/zh/02.Python");
+} else {
+    window.location.replace("https://xplanc.org/primers/document/en/02.Python");
+}
