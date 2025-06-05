37c260988f105fa6bb5740494b9aeef8de622bfd
Plan C <hubenchang0515@outlook.com>
2025-06-05 19:31:13 +0800

    重定向到主页面

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,1 @@
-if (navigator.language === 'zh-CN') {
-    window.location.replace("https://xplanc.org/primers/document/zh/02.Python");
-} else {
-    window.location.replace("https://xplanc.org/primers/document/en/02.Python");
-}
+window.location.replace("https://xplanc.org/primers/");
