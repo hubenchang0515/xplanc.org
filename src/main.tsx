deac28300b09ed45de811cdf9e18789dbaade27b
Plan C <hubenchang0515@outlook.com>
2025-06-08 18:07:50 +0800

    按语言重定向

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,5 @@
-window.location.replace("https://xplanc.org/primers/");
+if (navigator.language === 'zh-CN') {
+    window.location.replace("https://xplanc.org/primers/document/zh");
+} else {
+    window.location.replace("https://xplanc.org/primers/document/en");
+}
