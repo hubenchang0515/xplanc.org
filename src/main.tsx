44a144ba0e1e1d8f06b9eb3cce79a1a67744a1b3
Plan C <hubenchang0515@outlook.com>
2025-06-09 17:52:07 +0800

    暂时屏蔽英文页面，待补充英文内容后恢复

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-if (navigator.language === 'zh-CN') {
+// if (navigator.language === 'zh-CN') {
     window.location.replace("https://xplanc.org/primers/document/zh");
-} else {
-    window.location.replace("https://xplanc.org/primers/document/en");
-}
+// } else {
+//     window.location.replace("https://xplanc.org/primers/document/en");
+// }
